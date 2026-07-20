c789b9a02c9f7c018f0f71d7c9be6322e8b5a1ef
Mikri Akrida <zepamonha711@gmail.com>
2026-07-20 17:30:24 -0300

    Refatorando e correções

--- src/br/com/users/User.java
+++ src/br/com/users/User.java
@@ -4,21 +4,19 @@ import br.com.message.Message;
 
 import java.util.Scanner;
 
-public class User {
+public class User extends Message {
     static Scanner datesReader = new Scanner(System.in);
 
     public static int userInteraction() {
-        System.out.println(Message.exhibitMessages(0));
+        System.out.println(Message.getMessages().getFirst());
         int choice = datesReader.nextInt();
         datesReader.nextLine();
-
         return choice;
     }
 
     public static String testing() {
-        System.out.println(Message.exhibitMessages(1));
+        System.out.println(Message.getMessages().get(1));
         String choice = datesReader.nextLine();
-        datesReader.nextLine();
         return choice;
     }
 }
