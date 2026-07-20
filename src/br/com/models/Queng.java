0c6b4a7e985048ae8a2f1986cc78ca8edb4aa078
Mikri Akrida <zepamonha711@gmail.com>
2026-07-20 20:15:54 -0300

    Modificação e correções
    Alteração do nome da classe de executive para a pensada 'Queng'

--- src/br/com/models/Queng.java
+++ src/br/com/models/Queng.java
@@ -1,6 +1,6 @@
 package br.com.models;
 
-public class Executive extends Ticket {
+public class Queng extends Ticket {
     final String[] executiveElements = {"c4", "d4", "e4", "Nbd7", "cxd6", "Bxe5", "Rbc4", "Bd7", "Qh6+!!", "Kc4"};
 
     @Override
